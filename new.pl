32d6c40dfa11d708f539f767e733cb43a0b29b52
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-27 23:20:36 +0500

    facts
    another practise

---
+++ new.pl
@@ -0,0 +1,16 @@
+%X person teaches Y course
+
+%teaches(X,Y)
+teaches(drayaz,os).
+teaches(sirqasim,oop).
+teaches(sirhafeez,pf).
+teaches(sirayub,ooad).
+
+
+%X student studies Y course
+
+studies(raheel,os).
+studies(mumtaz,ooad).
+studies(raheel,daa).
+studies(sameer,pndc).
+
